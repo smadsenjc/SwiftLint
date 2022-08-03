4a59fb1f27034ad39e5d02af0bedab18affa62c4
Steve Madsen <steve.madsen@jumpcloud.com>
2022-08-03 11:00:19 -0400

    Initial Commit

---
+++ Sources/SwiftLint/SwiftLint.swift
@@ -0,0 +1,6 @@
+public struct SwiftLint {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftLintTests/SwiftLintTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftLint
+
+final class SwiftLintTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftLint().text, "Hello, World!")
+    }
+}
